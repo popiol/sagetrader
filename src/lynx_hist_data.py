2f41d12ca2fe682720f98ac4a08bced7713b0c6f
piotr.opiola <piotr.opiola@deepsense.ai>
2021-09-06 01:35:10 +0200

    lynx hist data

--- src/lynx_hist_data.py
+++ src/lynx_hist_data.py
@@ -4,7 +4,7 @@ import requests
 import json
 
 
-#resp = requests.post("https://localhost:5000/v1/api/iserver/secdef/search", json={"symbol": "KGH"}, verify=False)
+#resp = requests.post("https://localhost:5000/v1/api/iserver/secdef/search", json={"symbol": "AAPL"}, verify=False)
 #print(resp.text)
 #exit()
 
@@ -12,11 +12,19 @@ resp = requests.get("https://localhost:5000/v1/api/tickle", verify=False)
 print(resp.text)
 
 if not resp.text:
+    exit("Not connected")
+
+if resp.text == "error":
+    exit("Unknown error")
+
+if not resp.json()["iserver"]["authStatus"]["authenticated"]:
     exit("Not authenticated")
 
 session_id = resp.json()["session"]
 uri = "wss://api.ibkr.com/v1/api/ws"
 quotes = []
+error = ""
+errorcode = 0
 
 
 async def send(websocket, msg):
@@ -25,6 +33,7 @@ async def send(websocket, msg):
 
 
 async def handler():
+    global error, errorcode
     async with websockets.connect(uri) as websocket:
         req = json.dumps({"session": session_id})
         await send(websocket, req)
@@ -36,20 +45,33 @@ async def handler():
                 if resp["args"]["authenticated"]:
                     break
                 else:
+                    error = "Not authenticated"
+                    errorcode = 1
                     return
+            elif "error" in resp:
+                error = resp["error"]
+                errorcode = resp["code"]
+                return
 
-        await send(websocket, 'smh+13462029+{"source":"t"}')
-
+        #await send(websocket, 'smd+265598+{"fields":["0","1","2","3","4","5"]}')
+        await send(websocket, 'smh+265598+{"period":"3d","bar":"1d"}')
+        
         async for msg in websocket:
             print("<", msg)
             resp = json.loads(msg)
             if "symbol" in resp:
-                quotes.append(resp)
+                server_id = resp["serverId"]
+                quotes.append(resp["data"])
                 break
+            elif "error" in resp:
+                error = resp["error"]
+                errorcode = resp["code"]
+                return
 
-        await send(websocket, 'umh+'+quotes[-1]["serverId"])
+        await send(websocket, 'umh+'+server_id)
                 
 
 
 asyncio.run(handler())
-
+if errorcode > 0:
+    print("error", errorcode, error)
