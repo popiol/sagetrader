7e6225af137f1d80a98ab6b351cc2101256cd05d
piotr.opiola <piotr.opiola@deepsense.ai>
2021-10-19 02:36:03 +0200

    new model

--- src/custom_agent.py
+++ src/custom_agent.py
@@ -42,7 +42,12 @@ class CustomAgent:
     def create_hist_model(self):
         inputs = keras.layers.Input(shape=(self.max_quotes, 5))
         l = inputs
-        l = keras.layers.LSTM(1)(l)
+        l = keras.layers.LSTM(self.max_quotes)(l)
+        outputs = keras.layers.Dense(100, activation="relu")(l)
+        outputs = keras.layers.Dense(100, activation="relu")(l)
+        outputs = keras.layers.Dense(50, activation="relu")(l)
+        outputs = keras.layers.Dense(20, activation="relu")(l)
+        outputs = keras.layers.Dense(10, activation="relu")(l)
         outputs = keras.layers.Dense(1, activation="sigmoid")(l)
         model = keras.Model(inputs=inputs, outputs=outputs)
         model.compile(
@@ -54,7 +59,12 @@ class CustomAgent:
     def create_rt_model(self):
         inputs = keras.layers.Input(shape=(self.max_quotes, 6))
         l = inputs
-        l = keras.layers.LSTM(3)(l)
+        l = keras.layers.LSTM(self.max_quotes)(l)
+        outputs = keras.layers.Dense(100, activation="relu")(l)
+        outputs = keras.layers.Dense(100, activation="relu")(l)
+        outputs = keras.layers.Dense(50, activation="relu")(l)
+        outputs = keras.layers.Dense(20, activation="relu")(l)
+        outputs = keras.layers.Dense(10, activation="relu")(l)
         outputs = keras.layers.Dense(3, activation="sigmoid")(l)
         model = keras.Model(inputs=inputs, outputs=outputs)
         model.compile(
