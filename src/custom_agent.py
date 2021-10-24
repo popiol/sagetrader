cbeea4e330f0d8c3adff8f9a836fc1641b3ed290
piotr.opiola <piotr.opiola@deepsense.ai>
2021-10-24 11:37:23 +0200

    model

--- src/custom_agent.py
+++ src/custom_agent.py
@@ -42,7 +42,7 @@ class CustomAgent:
     def create_hist_model(self):
         inputs = keras.layers.Input(shape=(self.max_quotes, 5))
         l = inputs
-        l = keras.layers.LSTM(10)(l)
+        l = keras.layers.LSTM(self.max_quotes)(l)
         l = keras.layers.Dense(100, activation="relu")(l)
         l = keras.layers.Dense(10, activation="relu")(l)
         l = keras.layers.Dense(10, activation="relu")(l)
@@ -58,7 +58,7 @@ class CustomAgent:
     def create_rt_model(self):
         inputs = keras.layers.Input(shape=(self.max_quotes, 6))
         l = inputs
-        l = keras.layers.LSTM(10)(l)
+        l = keras.layers.LSTM(self.max_quotes)(l)
         l = keras.layers.Dense(100, activation="relu")(l)
         l = keras.layers.Dense(10, activation="relu")(l)
         l = keras.layers.Dense(10, activation="relu")(l)
