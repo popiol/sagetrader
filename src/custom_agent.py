df70e087fa5f0f75c84922740304e2d1280261b6
piotr.opiola <piotr.opiola@deepsense.ai>
2021-10-27 23:48:51 +0200

    model

--- src/custom_agent.py
+++ src/custom_agent.py
@@ -42,11 +42,8 @@ class CustomAgent:
     def create_hist_model(self):
         inputs = keras.layers.Input(shape=(self.max_quotes, 5))
         l = inputs
-        l = keras.layers.LSTM(self.max_quotes)(l)
+        l = keras.layers.LSTM(100)(l)
         l = keras.layers.Dense(100, activation="relu")(l)
-        l = keras.layers.Dense(10, activation="relu")(l)
-        l = keras.layers.Dense(10, activation="relu")(l)
-        l = keras.layers.Dense(10, activation="relu")(l)
         outputs = keras.layers.Dense(1, activation="sigmoid")(l)
         model = keras.Model(inputs=inputs, outputs=outputs)
         model.compile(
@@ -58,11 +55,8 @@ class CustomAgent:
     def create_rt_model(self):
         inputs = keras.layers.Input(shape=(self.max_quotes, 6))
         l = inputs
-        l = keras.layers.LSTM(self.max_quotes)(l)
+        l = keras.layers.LSTM(100)(l)
         l = keras.layers.Dense(100, activation="relu")(l)
-        l = keras.layers.Dense(10, activation="relu")(l)
-        l = keras.layers.Dense(10, activation="relu")(l)
-        l = keras.layers.Dense(10, activation="relu")(l)
         outputs = keras.layers.Dense(3, activation="sigmoid")(l)
         model = keras.Model(inputs=inputs, outputs=outputs)
         model.compile(
